275da336638bfd48c901a97d596260b7d513c1b9
Victor Batigin <bixs85@gmail.com>
2020-04-05 20:55:32 +0300

    add task

---
+++ src/js/color-switch.js
@@ -0,0 +1,38 @@
+const startBtnRef = document.querySelector('button[data-action="start"]');
+const stopBtnRef = document.querySelector('button[data-action="stop"]');
+const bodyRef = document.querySelector('body');
+
+startBtnRef.addEventListener('click', onStart);
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+let timerId = 0;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const index = () => randomIntegerFromInterval(colors.length, -1);
+
+function onStart() {
+  timerId = setInterval(addBgColor, 1000);
+  startBtnRef.removeEventListener('click', onStart);
+  stopBtnRef.addEventListener('click', onStop);
+}
+
+function addBgColor() {
+  bodyRef.setAttribute('style', `background-color: ${colors[index()]}`);
+}
+
+function onStop() {
+  clearInterval(timerId);
+  startBtnRef.addEventListener('click', onStart);
+  stopBtnRef.removeEventListener('click', onStop);
+}
